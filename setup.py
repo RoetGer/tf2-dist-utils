11ff622a9d5eae53589a0171af33e39772204169
Gernot Roetzer <gernot dot roetzer at  gmail dot com>
2021-02-14 00:56:36 +0100

    Add setup.py and __init__.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf2_dist_utils",
+    version="0.0.1",
+    url="https://github.com/RoetGer/tf2-dist-utils",
+    packages=find_packages(include=["tf2_dist_utils"])
+)
