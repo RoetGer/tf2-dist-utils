86f28205fd9cbca4ec2cc14a6b29c18fecca1d40
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-02-17 01:17:49 +0100

    Add docstrings to NegLogLikeLoss and build_loss.

--- tf2_dist_utils/losses.py
+++ tf2_dist_utils/losses.py
@@ -4,8 +4,24 @@ from tf2_dist_utils.distributions import TransNormal
 
 
 class NegLogLikeLoss(tf.keras.losses.Loss):
+    '''Base class for negative loglikelihood based losses'''
 
     def call(self, y_true, y_pred):
+        '''Computes log-probability of observations
+        
+        Parameters
+        ----------
+        y_true : tf.tensor
+            Observations
+        y_pred : tf.tensor
+            Parameter values of the distribution underlying the loss.
+        
+        Returns
+        -------
+        tf.tensor
+            Negative loglikelihood for each observation.
+        '''
+
         shape = y_pred.shape
         
         lst = tf.split(
@@ -17,6 +33,24 @@ class NegLogLikeLoss(tf.keras.losses.Loss):
         return -rv.log_prob(y_true)
 
 def build_loss(class_loss_name, dist):
+    '''Creates a tensorflow 2.x loss function based on NegLokLikeLoss
+
+    Parameters
+    ----------
+    class_loss_name : str
+        Name/Type of the loss to be created, e.g. "TransNormal"
+    dist : distribution object
+        Can be any type of tensorflow distribution object, 
+        which supports parameter specification via call 
+        (e.g. dist(param1, param2)) and has a log_prob method
+    
+    Returns
+    -------
+    Object of type class_loss_name
+        Loss object of type class_loss_name which can be used
+        in conjuction with tensorflow 2.x models.
+    '''
+    
     def __init__(self):
         self.dist = dist
         super(NegLogLikeLoss, self).__init__()
